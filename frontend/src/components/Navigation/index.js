15505ea607400b71ee79225e5c377a2cde509eed
carlos rodriguez <twincarlos98@gmail.com>
2022-03-03 14:08:00 -0500

    demo user added

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
 
   let sessionLinks;
@@ -15,16 +17,21 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink id='login-a' to="/login">Log In</NavLink>
+        <NavLink id='signup-a' to="/signup">Sign Up</NavLink>
+        <a id='demo-a' onClick={() => dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password' }))}>Demo</a>
       </>
     );
   }
 
   return (
-    <ul>
+    <ul id='nav'>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink id='home-a' exact to="/">Home</NavLink>
+        <form>
+          <button>S</button>
+          <input type='text' placeholder='Search events'></input>
+        </form>
         {isLoaded && sessionLinks}
       </li>
     </ul>
